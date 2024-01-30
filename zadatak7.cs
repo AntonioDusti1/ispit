095481161273a431532404f35c39b5d3d8ff6fcf
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-30 13:07:19 +0100

    Add files via upload

---
+++ zadatak7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        class KlassX
+        {
+            private int broj;
+            public int broj1
+            {
+                get(return broj);
+                set(broj= value);
+            }
+        }
+    class KlassX : KlassY
+    {
+
+    }
+        static void Main(string[] args)
+        {
+        KlassY y = new KlassY();
+        y.Broj = 10;
+
+        Console.WriteLine("Vrijednost broja je (y.broj)");
+        }
+    
+    }
+}
+}
